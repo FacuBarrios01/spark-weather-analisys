31b50b3bda8132fbe0bcdc97687cf5cd943c4da0
FacuBarrios01 <facubarrios@gmail.com>
2023-01-17 00:33:54 +0100

    CSVs generated

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,11 @@
-package lab.scala.lab8
-
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SparkSession.setActiveSession
-import org.apache.spark.sql.functions.{abs, avg, col, count, max, min, sqrt, sum}
+import org.apache.spark.sql.functions.{abs, avg, col, count, first, max, min, month, round, sqrt, sum, year}
 import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val OUTPUT_PATH = "/home/facu/IdeaProjects/lab8-sbt/src/main/resources/output"
     val spark = SparkSession.builder().master("local").getOrCreate()
     setActiveSession(spark)
 
@@ -18,6 +17,7 @@ object Main {
       StructField("humidity",DoubleType,false),
       StructField("windspeed",DoubleType,false)
     ))
+
     /*
     * Obtaining DataFrames, selecting our columns and applying schema
     * */
@@ -42,6 +42,13 @@ object Main {
           col("windspeed").cast(DoubleType).as("windspeed")
     )
 
+    valenciaDF.write.option("header",true)
+      .mode("overwrite")
+      .csv(OUTPUT_PATH + "/valenciaDF")
+    madridDF.write.option("header",true)
+      .mode("overwrite")
+      .csv(OUTPUT_PATH + "/madridDF")
+
     /*Temperature & Humidity month averages
     * We override datetime ignoring individual days in order to group our rows my individual months
     */
@@ -49,13 +56,17 @@ object Main {
       .groupBy("datetime")
       .avg("temp","humidity").sort("datetime")
       .sort("datetime")
+      .toDF()
     val madridMA = madridDF.withColumn("datetime", madridDF("datetime").substr(0,7))
       .groupBy("datetime")
       .avg("temp","humidity")
       .sort("datetime")
+      .toDF()
 
+    valenciaMA.write.option("header",true).mode("overwrite").csv(OUTPUT_PATH + "/valenciaMA")
+    madridMA.write.option("header",true).mode("overwrite").csv(OUTPUT_PATH + "/madridMA")
     /*Wind averages
-     */
+     */  
     val windAvgValencia = valenciaDF.withColumn("datetime", valenciaDF("datetime").substr(0, 7))
       .groupBy("datetime")
       .agg(
@@ -68,6 +79,8 @@ object Main {
         avg("windspeed").as("windAverage"),
         sqrt(sum("windspeed") / count("windspeed")).as("windVectorAvg")
       ).sort("datetime")
+    windAvgMadrid.write.option("header",true).mode("overwrite").csv(OUTPUT_PATH + "/madridWAV")
+    windAvgValencia.write.option("header",true).mode("overwrite").csv(OUTPUT_PATH + "/valenciaWAV")
 
     /*Monthly min & max temp
     */
@@ -83,19 +96,28 @@ object Main {
         min("temp").as("tempMin"),
         max("temp").as("tempMax")
       ).sort("datetime")
+    minMaxMadrid.write.option("header",true).mode("overwrite").csv(OUTPUT_PATH + "/madridMinMaxT")
+    minMaxValencia.write.option("header",true).mode("overwrite").csv(OUTPUT_PATH + "/valenciaMinMaxT")
 
     /*Differences in temperatures between costal and inner land city
     * */
     val reducedMDF = madridDF.select("datetime", "temp")
       .withColumn("mtemp", col("temp"))
       .withColumn("mDatetime", col("datetime"))
+      .drop("datetime","temp")
     val reducedVDF = valenciaDF.select("datetime", "temp")
       .withColumn("vtemp", col("temp"))
       .withColumn("vDatetime", col("datetime"))
+      .drop("datetime", "temp")
 
-    val joinedDF = reducedVDF.join(reducedMDF , col("vDatetime") === col("mDatetime") , "inner")
+    val diffTempsDF = reducedVDF.join(reducedMDF , col("vDatetime") === col("mDatetime") , "right")
       .withColumn("datetime", col("vDatetime").substr(0, 7))
-      .groupBy(col("datetime"))
-      .agg(abs(col("vtemp") - col("mtemp")).as("Difference") )
+      .drop("vDatetime", "mDatetime")
+      .withColumn("difference",
+        round(abs(reducedMDF.col("mtemp") - reducedVDF.col("vtemp")))
+      )
+
+    val monthlyMaxDiff = diffTempsDF.groupBy("datetime").max("difference")
+    monthlyMaxDiff.write.option("header",true).mode("overwrite").csv(OUTPUT_PATH + "/monthlyMaxDif")
   }
 }
